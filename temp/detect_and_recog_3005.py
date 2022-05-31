590fa9b6aa8fa90e930052b6cf3672e01be9701c
iamsvp <PSV1994@uandex.ru>
2022-05-31 09:53:36 +0500

    ready pipeline with face crop normalizing

--- temp/detect_and_recog_3005.py
+++ temp/detect_and_recog_3005.py
@@ -8,21 +8,87 @@ from tqdm import tqdm
 from pathlib import Path
 import matplotlib.pyplot as plt
 import random
+from PIL import Image
+import math
 
 PARENT_DIR = Path('/home/psv/PycharmProjects/insightface/')
 
-padding = 0
-padding_type = '%'
 align = True
-mode = 'mean'
-metric = 'cosine'
-recog_tresh = 0.725
-turnmetric = 10
+recog_tresh = 0.65
+turnmetric = 8
 bright_etalon = 150
+n_persons = 1505
 
-new_img_dir_path = PARENT_DIR / 'temp' / f'bright4_1305_turn={turnmetric}_recog_tresh={recog_tresh}'
+new_img_dir_path = PARENT_DIR / 'temp' / f'bright_{bright_etalon}_3005_n_persons={n_persons}_turn={turnmetric}_recog_tresh={recog_tresh}'
 new_img_dir_path.mkdir(exist_ok=True, parents=True)
 print(f'save to {new_img_dir_path}')
+
+
+def get_random_color():
+    randomcolor = (random.randint(100, 255), random.randint(100, 255), random.randint(0, 100))
+    return randomcolor
+
+
+def findEuclideanDistance(source_representation, test_representation):
+    euclidean_distance = source_representation - test_representation
+    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
+    euclidean_distance = np.sqrt(euclidean_distance)
+    return euclidean_distance
+
+
+# this function copied from the deepface repository: https://github.com/serengil/deepface/blob/master/deepface/commons/functions.py
+def alignment_procedure(img, landmark):
+    (right_eye_x, right_eye_y), (left_eye_x, left_eye_y), nose, _, _ = landmark.astype(np.int32)  # (X,Y)
+    # -----------------------
+    upside_down = False
+    if nose[1] < left_eye_y or nose[1] < right_eye_y:
+        upside_down = True
+    # -----------------------
+    # find rotation direction
+
+    if left_eye_y > right_eye_y:
+        point_3rd = (right_eye_x, left_eye_y)
+        direction = -1  # rotate same direction to clock
+    else:
+        point_3rd = (left_eye_x, right_eye_y)
+        direction = 1  # rotate inverse direction of clock
+
+    # -----------------------
+    # find length of triangle edges
+
+    a = findEuclideanDistance(np.array((left_eye_x, left_eye_y)), np.array(point_3rd))
+    b = findEuclideanDistance(np.array((right_eye_x, right_eye_y)), np.array(point_3rd))
+    c = findEuclideanDistance(np.array((right_eye_x, right_eye_y)), np.array((left_eye_x, left_eye_y)))
+
+    # -----------------------
+
+    # apply cosine rule
+
+    if b != 0 and c != 0:  # this multiplication causes division by zero in cos_a calculation
+
+        cos_a = (b * b + c * c - a * a) / (2 * b * c)
+
+        # PR15: While mathematically cos_a must be within the closed range [-1.0, 1.0], floating point errors would produce cases violating this
+        # In fact, we did come across a case where cos_a took the value 1.0000000169176173, which lead to a NaN from the following np.arccos step
+        cos_a = min(1.0, max(-1.0, cos_a))
+
+        angle = np.arccos(cos_a)  # angle in radian
+        angle = (angle * 180) / math.pi  # radian to degree
+
+        # -----------------------
+        # rotate base image
+
+        if direction == -1:
+            angle = 90 - angle
+
+        if upside_down == True:
+            angle = angle + 90
+
+        img = Image.fromarray(img)
+        img = np.array(img.rotate(direction * (-angle)))
+    # -----------------------
+
+    return img  # return img anyway
 
 
 class ArcFaceONNXVL(ArcFaceONNX):
@@ -31,8 +97,7 @@ class ArcFaceONNXVL(ArcFaceONNX):
 
     def get(self, img, face=None, show=False):
         if face:
-            from insightface.utils import face_align
-            aimg = face_align.norm_crop(img, landmark=face.kps)
+            aimg = alignment_procedure(img, landmark=face.kps)
         else:
             aimg = cv2.resize(img, (112, 112))
         if show:
@@ -91,7 +156,9 @@ recognator.prepare(ctx_id=0)
 
 
 class Person:
-    def __init__(self, path=None, img=None, label='Unknown', color=(0, 0, 255), emb_net=recognator,
+    def __init__(self, path=None, img=None,
+                 makemask=False, align=True,
+                 label='Unknown', color=(0, 0, 255), emb_net=recognator,
                  change_brightness=True, show=False):
         self.path = str(path) if path else None
         self.color = color
@@ -100,6 +167,15 @@ class Person:
             img = cv2.imread(self.path)
         else:
             img = img
+        if makemask:
+            faces = detector.get(img)
+            face = faces[0]
+            # assert len(faces) == 1, f'"{len(faces)}" persons on the photo "{self.path}"'  # TODO: add condition
+            box = face.bbox.astype(np.int32)
+            img = img[box[1]:box[3], box[0]:box[2]]
+            if align:
+                img = alignment_procedure(img, landmark=face.kps)
+
         if change_brightness:
             self.img = self.change_brightness(img, etalon=bright_etalon)
         else:
@@ -112,7 +188,7 @@ class Person:
         self.embedding = self._get_embedding()
 
     def _get_embedding(self):
-        return self.emb_net.get(self.img)
+        return self.emb_net.get(self.img, show=False)
 
     def get_label(self, persons, threshold=0.7, metric='cosine', face=None, full_img=None, show=False):
         dists = []
@@ -198,41 +274,68 @@ class Person:
         return turn
 
 
-DIR = Path('/home/psv/file/project/recog_datasets/LABELED_FACES/CROP_FACES/')
-person1 = Person(path=DIR / 'Sergei/fas.jpg', label='Sergei', color=(0, 255, 0), show=False)
-person2 = Person(path=DIR / 'Vladislav/fas.jpg', label='Vladislav', color=(255, 0, 0), show=False)
-person3 = Person(path=DIR / 'Putin/fas.jpg', label='Putin', color=(113, 47, 38), show=False)
-person4 = Person(path=DIR / 'Irina/fas.jpg', label='Irina', color=(31, 58, 61), show=False)
-person5 = Person(path=DIR / 'Korzh/fas.jpg', label='Korzh', color=(137, 40, 80), show=False)
-person6 = Person(path=DIR / 'Bruce/fas.jpg', label='Bruce', color=(180, 40, 137), show=False)
-person7 = Person(path=DIR / 'Farid/fas.jpg', label='Farid', color=(180, 237, 140), show=False)
-persons = [person1, person2, person3, person4, person5, person6, person7]
+show_persons = False
+all_persons = []
+PERSON_DIR = Path('/home/psv/Downloads/datasets/lfw1img/')
+person_dirs = list(PERSON_DIR.glob('*'))
+p_bar = tqdm(person_dirs[:n_persons], colour='yellow')
+for dir in p_bar:
+    p_bar.set_description(f'{str(dir)}')
+    img_path = list(dir.glob('*.jpg'))[0]
+    label = img_path.parts[-2]
+    person = Person(path=img_path, label=label, color=get_random_color(),
+                    makemask=True, change_brightness=True,
+                    show=show_persons)
+    all_persons.append(person)
+
+DIR = Path('/home/psv/file/project/recog_datasets/LABELED_FACES/LABELED_full/')
+person1 = Person(path=DIR / 'Sergei/fas.jpg', label='Sergei', color=(0, 255, 0), makemask=True, show=show_persons)
+person2 = Person(path=DIR / 'Vladislav/fas.jpg', label='Vladislav', color=(255, 0, 0), makemask=True, show=show_persons)
+person3 = Person(path=DIR / 'Farid/fas.jpg', label='Farid', color=(180, 237, 140), makemask=True, show=show_persons)
+person4 = Person(path=DIR / 'Denis/fas.jpg', label='Denis', color=(80, 127, 200), makemask=True, show=show_persons)
+person5 = Person(path=DIR / 'Anton/fas.jpg', label='Anton', color=(213, 147, 138), makemask=True, show=show_persons)
+person6 = Person(path=DIR / 'Alexandr/fas.jpg', label='Alexandr', color=(131, 158, 101), makemask=True, show=show_persons)
+person7 = Person(path=DIR / 'Putin/fas.jpg', label='Putin', color=(113, 47, 38), makemask=True, show=show_persons)
+person8 = Person(path=DIR / 'Irina/fas.jpg', label='Irina', color=(31, 58, 61), makemask=True, show=show_persons)
+person9 = Person(path=DIR / 'Korzh/fas.jpg', label='Korzh', color=(137, 40, 80), makemask=True, show=show_persons)
+person10 = Person(path=DIR / 'Bruce/fas.jpg', label='Bruce', color=(180, 40, 137), makemask=True, show=show_persons)
+all_persons.extend([person1, person2, person3, person4, person5, person6, person7, person8, person9, person10])
 
 if __name__ == '__main__':
-    DATASET_DIR = Path('/home/psv/Pictures/mpv/goods_cooler/')
-    # imgs = list(DATASET_DIR.glob('**/*.jpg'))
-    imgs = list(DATASET_DIR.glob('*.jpg'))
+    # DATASET_DIR = Path('/home/psv/Pictures/mpv/goods_cooler/')
+    # imgs = list(DATASET_DIR.glob('*.jpg'))
+    DATASET_DIR = Path('/home/psv/Pictures/mpv/office_cooler_persons/')
+    imgs = list(DATASET_DIR.glob('**/*.jpg'))
 
-    random.shuffle(imgs)
+    # imgs.extend(list(PERSON_DIR.glob('**/*.jpg')))
 
-    p_bar = tqdm(imgs)
+    p_bar = tqdm(imgs, colour='green')
     for img_idx, img_path in enumerate(p_bar):
         p_bar.set_description(f'{img_path}')
         img = cv2.imread(str(img_path))
+
+        # img = cv2.flip(img, 1)  # TODO: del this str!!!
+
         faces = detector.get(img)
         whoes = []
         for face_idx, face in enumerate(faces):
             emb = np.expand_dims(face['embedding'], axis=0)
             box = face.bbox.astype(np.int32)
             crop_face = img[box[1]:box[3], box[0]:box[2]]
-            crop_face = Person.change_brightness(crop_face, etalon=bright_etalon)
+            try:
+                crop_face = Person.change_brightness(crop_face, etalon=bright_etalon)
+            except cv2.error:
+                continue
             unknown = Person(img=crop_face, change_brightness=True, show=False)
             face.brightness = unknown.brightness
-            near_dist = unknown.get_label(persons, threshold=recog_tresh, full_img=img, face=face, show=False)
+            near_dist = unknown.get_label(all_persons, threshold=recog_tresh, full_img=img, face=face, show=False)
+            if unknown.label != 'Unknown':
+                print('\n', img_path, unknown.label)
             whoes.append((unknown.label, round(near_dist, 4), unknown.color))
-        dimg = detector.draw_on(img, faces, whoes=whoes, show_kps=False, show=False)
-        cv2.imwrite(str(new_img_dir_path / f'{img_path.name}'), dimg)
-        # exit()
-        if img_idx > 50:
-            exit()
+
+        if len(faces) == len(whoes):
+            dimg = detector.draw_on(img, faces, whoes=whoes, show_kps=False, show=False)
+            cv2.imwrite(str(new_img_dir_path / f'{img_path.name}'), dimg)
+        # if img_idx > 50:
+        #     exit()
 # '''
